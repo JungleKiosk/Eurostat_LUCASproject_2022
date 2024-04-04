a97c2eb21a68dc0e6b0defd51ecbd83fd2415a64
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-04 23:26:21 +0200

    setting Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,96 @@
-<script setup>
+<script>
+import { goalsArray } from "../data/goalsOnuGrid.js"
+
+export default {
+    name: "Home",
+    data() {
+        return {
+            goalsArray
+        }
+    },
+    methods: {
+        getImagePath: function (name) {
+            return new URL(`../assets/img/${name}`, import.meta.url).href
+        }
+    }
+}
 
 
 </script>
 
 
 <template>
+    <main>
+        <section class="jumbotron">
 
-<h1>Hello Home!</h1>
+            <div class="container">
+                <div class="row justify-content-center">
 
+                    <div class="col-12 col-lg-6 bg_row_img py-5 my-2 align-content-center introduce">
+                        <h1><strong>Hi, I'm </strong><span class="txt_title_thin">FRA</span></h1>
+                        <p>
+                            I have a master's degree in Planning and Management of Agro-Territorial, Forest and
+                            Landscape:
+                            <a href="https://corsi.unibo.it/2cycle/LandscapePlanningManagement" target="_blank"><span
+                                    class="txt_primary color_txt_link"> PROGESA</span> </a><br>
+                            The study program contribute to the pursuit of the <br>
+                            UN 2030 Agenda for
+                            <a href="https://www.un.org/sustainabledevelopment/" target="_blank"><span
+                                    class="txt_primary color_txt_link" target="_blank">Sustainable
+                                    Development Goals.</span> </a> <br>
+                            I have a passion for the environment and for technology.<br>
+                        </p>
+                        <hr>
+                        <p>
+                            In April 2022 I accepted an assignment from <a href="https://ec.europa.eu/eurostat"><span
+                                    class="txt_primary color_txt_link">Eurostat</span></a> to
+                            carry out
+                            data collection for the <span class="txt_primary">LUCAS2022</span> project.
+                        </p>
+                        <p>
+                            On this page you will find the explanation of how to optimize the work of the <span
+                                class="txt_primary">LUCAS Detector</span>;
+                            through the use of open source software.
+                        </p>
+                        <p>
+                            If you want to know more about the LUCAS project click the button below.
+                        </p>
+
+                        <div class="link_lucas text-center py-2">
+                            <a href="https://ec.europa.eu/eurostat/web/lucas"
+                                class="btn text-light zoom-effect lucas_link" target="_blank"><strong>LUCAS
+                                    website</strong></a>
+                        </div>
+
+
+                    </div>
+
+                    <div class="col-12 col-lg-4 d-flex flex-wrap goals mx-4 col_goals_onu">
+
+                        <div class="col-3 text-center p-2" v-for="goal in goalsArray" :key="goal.img">
+                            <a :href="goal.link" target="_blank">
+                                <img class="rounded-4 zoom-effect-goals" :src="getImagePath(goal.img)" alt="Goal Image">
+                            </a>
+
+                        </div>
+
+                    </div>
+
+                    
+
+
+
+                </div>
+
+
+
+            </div>
+
+        </section>
+
+
+
+    </main>
 </template>
 
-<style>
-
-</style>
+<style></style>
