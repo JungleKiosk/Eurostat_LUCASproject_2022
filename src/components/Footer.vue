3173367eb104dad3f2ede6f994fc27bc68d56206
Fra <135766845+JungleKiosk@users.noreply.github.com>
2024-04-05 08:54:37 +0200

    component: footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+
+</script>
+
+
+<template>
+    <footer id="footer" class="p-5">
+        <div class="row mt-5">
+            <h4>Contacts</h4>
+            <div class="col-3">
+                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
+                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
+                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
+            </div>
+            <div class="col-3">
+                <p>
+                    <img class="icon_contact" src="../assets/img/logo/mail.png" alt="">
+                    eso.fra24@gmail.com
+                </p>
+            </div>
+
+            <div class="col-6">
+
+            </div>
+
+        </div>
+    </footer>
+</template>
+
+<style scoped>
+
+#footer {
+
+background-color: #000000;
+}
+
+.icon_github {
+    width: 20%;
+}
+
+.icon_contact {
+    width: 30%;
+}
+
+@media (max-width: 786px) {
+.icon_contact {
+    width: 60%;
+}
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,7 @@
 <script>
 import { goalsArray } from "../data/goalsOnuGrid.js"
+import Footer from '../components/Footer.vue';
+
 
 export default {
     name: "Home",
@@ -7,6 +9,9 @@ export default {
         return {
             goalsArray
         }
+    },
+    components: {
+        Footer
     },
     methods: {
         getImagePath: function (name) {
@@ -159,29 +164,7 @@ export default {
             </div>
         </section>
     </main>
-    <footer id="footer" class="p-5">
-        <div class="row mt-5">
-            <h4>Contacts</h4>
-            <div class="col-3">
-                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
-                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
-                <img class="icon_github" src="../assets/img/logo/github_footer.svg" alt="">
-
-
-            </div>
-            <div class="col-3">
-                <p>
-                    <img class="icon_contact" src="../assets/img/logo/mail.png" alt="">
-                    eso.fra24@gmail.com
-                </p>
-            </div>
-
-            <div class="col-6">
-
-            </div>
-
-        </div>
-    </footer>
+    <Footer></Footer>
 </template>
 
 <style scoped>
@@ -189,24 +172,6 @@ export default {
     width: 50%;
 }
 
-.icon_github{
-    width: 20%;
-}
 
 
-
-.icon_contact {
-    width: 30%;
-}
-
-#footer {
-
-    background-color: #000000;
-}
-
-@media (max-width: 786px) {
-    .icon_contact {
-        width: 60%;
-    }
-}
 </style>
